d4f64b7c19eafa1b69897bded30a441b9522e5d8
Danyel555 <113988414+Danyel555@users.noreply.github.com>
2022-09-28 22:12:35 +0300

    Update and rename Githard/Program.cs to Гитхард/Program.cs

---
+++ Гитхард/Program.cs
@@ -0,0 +1,27 @@
+Console.WriteLine("1. Сложить 2 числа\l\n2. Вычесть первое из второго\l\n3. Перемножить два числа\l\n4. Разделить первое на второе\l\n5. Возвести в степень N первое число\l\n6. Найти квадратный корень из числа\l\n7. Найти 1 процент от числа\l\n8. Найти факториал из числа\l\n9. Выйти из программы\l\n");
+int l = 1; 
+Console.WriteLine("выберите операцию"); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+while (n == 9) ; 
+Console.WriteLine("1. Сложить 2 числа\l\n2. Вычесть первое из второго\l\n3. Перемножить два числа\l\n4. Разделить первое на второе\l\n5. Возвести в степень N первое число\l\n6. Найти квадратный корень из числа\l\n7. Найти 1 процент от числа\l\n8. Найти факториал из числа\l\n9. Выйти из программы\l\n"); 
+int l = 1; 
+Console.WriteLine("выберите операцию"); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+while (n == 9) ;
+{ Console.WriteLine("введите 1-ое число"); 
+    int a = Convert.ToInt32(Console.ReadLine()); 
+    Console.WriteLine("введите 2-ое число"); 
+    int b = Convert.ToInt32(Console.ReadLine()); 
+    if (n == 1) { l = a + b; } 
+    else if (n == 2) { l = b - a; } 
+    else if (n == 3) { l = b * a; }
+    else if (n == 4) { l = a / b; }
+    else if (n == 5) { l = Convert.ToInt32(Math.Pow(a, 2)); } 
+    else if (n == 6) { l = Convert.ToInt32(Math.Sqrt(a)); } 
+    else if (n == 7) { l = a / 100; } 
+    else if (n == 8) { for (int i = a; i > 1; i--) { l = l * i; } } 
+    else { Console.WriteLine("Неизвестный оператор"); } 
+    Console.WriteLine("ответ");
+    Console.WriteLine(l); 
+    Console.WriteLine("выберите операцию"); 
+    n = Convert.ToInt32(Console.ReadLine()); }
